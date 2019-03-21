2ace48ac6ce40dc4588398842d1b76a8a558f26d
Jason Hochheiser <jsunh98@gmail.com>
2019-03-21 16:01:50 -0400

    .gitignore node_modules

---
+++ src/main/java/org/wecancodeit/nfl/NflApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.nfl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NflApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wecancodeit/nfl/NflApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.nfl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NflApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
